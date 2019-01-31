4439628024c07c114ec6085bab752a4828de01e0
Michael King-Stockton <kingstockton@gmail.com>
2019-01-31 01:57:27 -0700

    added test for player and game

--- src/Box.js
+++ src/Box.js
@@ -1,36 +1,36 @@
 import domUpdates from './domUpdates.js';
 
-class Box {
-  constructor(height, width){
-    this.height = height || 100;
-    this.width = width || 100;
-  }
+// class Box {
+//   constructor(height, width){
+//     this.height = height || 100;
+//     this.width = width || 100;
+//   }
 
-  area(){
-    return this.height * this.width;
-  }
+//   area(){
+//     return this.height * this.width;
+//   }
 
-  increaseWidth(amount){
-    let incrementAmount = this.width + amount;
-    return incrementAmount;
-  }
+//   increaseWidth(amount){
+//     let incrementAmount = this.width + amount;
+//     return incrementAmount;
+//   }
 
-  increaseHeight(amount){
-    this.height = this.height + amount;
-    domUpdates.displayHeight(this.height)
-    return this.height;
-  }
+//   increaseHeight(amount){
+//     this.height = this.height + amount;
+//     domUpdates.displayHeight(this.height)
+//     return this.height;
+//   }
 
-  increment(amount, boxPart){
-    let incrementAmount;
-    if(boxPart === 'height') {
-     incrementAmount = this.height + amount;
-    } else {
-     incrementAmount = this.width + amount;
-    }
-    // console.log(incrementAmount);
-    return incrementAmount
-  }
-}
+//   increment(amount, boxPart){
+//     let incrementAmount;
+//     if(boxPart === 'height') {
+//      incrementAmount = this.height + amount;
+//     } else {
+//      incrementAmount = this.width + amount;
+//     }
+//     // console.log(incrementAmount);
+//     return incrementAmount
+//   }
+// }
 
 export default Box;
